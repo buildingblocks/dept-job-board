f25270fcc74988eacb388ac46febcf1a6b9400c0
Andrew Blackledge <a.blackledge@building-blocks.com>
2017-04-19 16:56:51 +0100

    adding a little more detail

--- src/Components/job-detail/job-detail.js
+++ src/Components/job-detail/job-detail.js
@@ -9,7 +9,10 @@ class JobDetail extends Component {
             <div>
                 <button className="btn btn--style-a back-btn" onClick={this.props.clearJobDetail}>Back</button>
                 <div onClick={this.props.clearJobDetail}>
-                    <h3>{JobDetail.title}</h3>
+                    <h3><a href={JobDetail.absolute_url} target="_blank">{JobDetail.title}</a></h3>
+                    {JobDetail.location.name ? <p>Location: {JobDetail.location.name}</p> : null}
+                    {JobDetail.departments[0].name ? <p>Department: {JobDetail.departments[0].name}</p> : null}
+                    {JobDetail.offices[0].name ? <p><strong>{JobDetail.offices[0].name}</strong></p> : null}
 
                 </div>
             </div>
